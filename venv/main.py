51d924be4a0c2bbf0a93d476b4c557a9cb929d63
Rayan Imani <rayanimani@gmail.com>
2024-04-11 13:35:37 -0500

    added some comments

--- venv/main.py
+++ venv/main.py
@@ -9,18 +9,18 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 
 
 
-# Email Configuration
+# Setting up Email credentials
 sender_email = "testiclops11@gmail.com"
 receiver_email = "rayanimani@gmail.com"
-password = "JellyFutbol11!"  # Consider using an app-specific password for security
+password = "JellyFutbol11!"  # The password for your email account
 smtp_server = "smtp.gmail.com"
-smtp_port = 587  # For Gmail with STARTTLS
+smtp_port = 587  # For Gmail 
 
 
 #send email function
 def send_email(subject, body):
     sg = sendgrid.SendGridAPIClient('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')
-    from_email = Email("rayanimani@gmail.com")  # Your SendGrid account email
+    from_email = Email("rayanimani@gmail.com")  # The SendGrid account email
     to_email = To("rayanimani@gmail.com")  # Where you want to send the email
     content = Content("text/plain", body)
     mail = Mail(from_email, to_email, subject, content)
@@ -70,7 +70,7 @@ time.sleep(3)
 for course in courseID:
     class_row = driver.find_element(By.ID, course)
     status_elements = class_row.find_elements(By.CLASS_NAME, 'section-closed')
-    course_name_element = class_row.find_elements(By.TAG_NAME, "td")[3]  # Adjust index as necessary
+    course_name_element = class_row.find_elements(By.TAG_NAME, "td")[3]  # Adjust index for different names
     course_name = course_name_element.text
 
     if status_elements:
